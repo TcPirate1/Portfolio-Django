c68ac3cfb5520ede52485d971256df2c5cae6eca
TC_linux <88120195+TcPirate1@users.noreply.github.com>
2022-12-28 11:53:05 +1300

    Changed URL in urls.py & created about page

--- Website/urls.py
+++ Website/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', homeView.as_view(), name='home'),
-    path('', projectsView.as_view(), name='projects'),
-    path('', aboutView.as_view(), name='about'),
+    path('projects/', projectsView.as_view(), name='projects'),
+    path('about/', aboutView.as_view(), name='about'),
 ]
