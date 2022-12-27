e2faacd5de28cc3ef7a326dcfd2bfdbb15fa2c79
TC_linux <88120195+TcPirate1@users.noreply.github.com>
2022-12-28 12:29:43 +1300

    Images now show after removing / from urls.py paths

--- Website/urls.py
+++ Website/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', homeView.as_view(), name='home'),
-    path('projects/', projectsView.as_view(), name='projects'),
-    path('about/', aboutView.as_view(), name='about'),
+    path('projects', projectsView.as_view(), name='projects'),
+    path('about', aboutView.as_view(), name='about'),
 ]
