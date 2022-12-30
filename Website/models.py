55aff0a5cee5b49b53953c22c1654777ec121c75
TC_windows <88120195+TcPirate1@users.noreply.github.com>
2022-12-31 00:09:24 +1300

    Setup models.py

--- Website/models.py
+++ Website/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.model):
+    Title = models.CharField(max_length=100)
+
+    Non_responsive_website = "Website: Non-Responsive"
+    responsive_website = "Website: Responsive"
+    program_non_gui = "Program: Non-GUI"
+    program_gui = "Program: GUI"
+    category_choices = [
+        ("empty", "EMPTY"),
+        (Non_responsive_website, "Website: Non-Responsive"),
+        (responsive_website,"Website: Responsive"),
+        (program_gui,"Program: Non-GUI"),
+        (program_gui,"Program: GUI"),
+    ]
+    category = models.CharField(
+        choices = category_choices,
+        default = "empty",
+        blank = False, #makes it a required field
+    )
+
+    URL = models.URLField(max_length=200)
+    language_framework = models.CharField(max_length=30)
