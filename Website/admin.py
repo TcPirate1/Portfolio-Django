cbe719417bf122c2c6b0cacb7874fe3a5afb153d
TC_windows <88120195+TcPirate1@users.noreply.github.com>
2023-01-03 20:27:31 +1300

    Edited some web pages and the admin dashboard.

--- Website/admin.py
+++ Website/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Categories
 
+class CategoriesAdmin(admin.ModelAdmin):
+    list_display = ('Title','category','language_framework')
+
 # Register your models here.
-admin.site.register(Categories)
+admin.site.register(Categories, CategoriesAdmin)
