f989c4e41d24e1f3a9e40d1669adb3a88b947343
TC_linux <88120195+TcPirate1@users.noreply.github.com>
2023-01-30 10:12:21 +1300

    Rearranged code for readability

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -24,12 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
+ALLOWED_HOSTS = []
 
-# ALLOWED_HOSTS = []
-DEBUG = False
-
-ALLOWED_HOSTS = ['tcdjangosite.pythonanywhere.com']
+# DEBUG = False
+# ALLOWED_HOSTS = ['tcdjangosite.pythonanywhere.com']
 
 
 # Application definition
