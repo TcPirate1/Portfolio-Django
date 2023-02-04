9acb5ef931f0ed9aacedbb59d851f810b0485fe0
TC_linux <88120195+TcPirate1@users.noreply.github.com>
2023-02-04 18:57:31 +1300

    Getting rid of stray code that is not needed. Like checking for debug state to load media folder

--- Website/views.py
+++ Website/views.py
@@ -15,8 +15,6 @@ class projectsView(ListView):
 class projectsDetail(DetailView):
     model = Categories
     template_name = "project_detail.html"
-    # def display_image(request):
-    #     getImages = Categories.image.objects.all()
 
 class aboutView(TemplateView):
     template_name = "about.html"
